ca280b0879ff81df0dd298e66cb454d79bf94c61
xynay <xiregiji@gmail.com>
2024-07-26 20:50:54 +0800

    Update route.tsx

--- app/api/favicon/route.tsx
+++ app/api/favicon/route.tsx
@@ -13,13 +13,13 @@ function getKey(url: string) {
 
 const faviconMapper: { [key: string]: string } = {
   '((?:zolplay.cn)|(?:zolplay.com)|(?:cn.zolplay.com))':
-    'https://cali.so/favicons/zolplay.png',
-  '(?:github.com)': 'https://cali.so/favicons/github.png',
+    'https://xinrengui.eu.org/favicons/zolplay.png',
+  '(?:github.com)': 'https://xinrengui.eu.org/favicons/github.png',
   '((?:t.co)|(?:twitter.com)|(?:x.com))':
-    'https://cali.so/favicons/twitter.png',
-  'coolshell.cn': 'https://cali.so/favicons/coolshell.png',
-  'vercel.com': 'https://cali.so/favicons/vercel.png',
-  'nextjs.org': 'https://cali.so/favicons/nextjs.png',
+    'https://xinrengui.eu.org/favicons/twitter.png',
+  'coolshell.cn': 'https://xinrengui.eu.org/favicons/coolshell.png',
+  'vercel.com': 'https://xinrengui.eu.org/favicons/vercel.png',
+  'nextjs.org': 'https://xinrengui.eu.org/favicons/nextjs.png',
 }
 
 function getPredefinedIconForUrl(url: string): string | undefined {
@@ -63,7 +63,7 @@ export async function GET(req: NextRequest) {
     return NextResponse.error()
   }
 
-  let iconUrl = 'https://cali.so/favicon_blank.png'
+  let iconUrl = 'https://xinrengui.eu.org/favicon_blank.png'
 
   try {
     const predefinedIcon = getPredefinedIconForUrl(url)
