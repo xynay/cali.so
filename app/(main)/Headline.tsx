3835f7bc637626846224c5fae4d6453798f805fc
xynay <xiregiji@gmail.com>
2024-07-27 14:08:31 +0800

    Update Headline.tsx

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import React from 'react'
-import { motion } from 'framer-motion'
 import Balancer from 'react-wrap-balancer'
+import { motion } from 'framer-motion'
 
 import { SparkleIcon, UserSecurityIcon } from '~/assets'
 
