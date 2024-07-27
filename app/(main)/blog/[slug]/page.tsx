00e5ca510f9dfdde6010e741621b3b821b01988e
xynay <xiregiji@gmail.com>
2024-07-28 01:18:36 +0800

    Update page.tsx

--- app/(main)/blog/[slug]/page.tsx
+++ app/(main)/blog/[slug]/page.tsx
@@ -65,27 +65,6 @@ export default async function BlogPage({
     views = 30578
   }
 
-  let reactions: number[] = []
-  try {
-    if (env.VERCEL_ENV === 'production') {
-      const res = await fetch(url(`/api/reactions?id=${post._id}`), {
-        next: {
-          tags: [`reactions:${post._id}`],
-        },
-      })
-      const data = await res.json()
-      if (Array.isArray(data)) {
-        reactions = data
-      }
-    } else {
-      reactions = Array.from({ length: 4 }, () =>
-        Math.floor(Math.random() * 50000)
-      )
-    }
-  } catch (error) {
-    console.error(error)
-  }
-
   let relatedViews: number[] = []
   if (typeof post.related !== 'undefined' && post.related.length > 0) {
     if (env.VERCEL_ENV === 'development') {
@@ -101,7 +80,6 @@ export default async function BlogPage({
       post={post}
       views={views}
       relatedViews={relatedViews}
-      reactions={reactions.length > 0 ? reactions : undefined}
     />
   )
 }
