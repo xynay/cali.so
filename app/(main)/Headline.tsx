493d4f063638f53793bf01122953a739b21762a5
xynay <xiregiji@gmail.com>
2024-07-27 13:59:32 +0800

    Update Headline.tsx

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -5,41 +5,31 @@ import Balancer from 'react-wrap-balancer'
 
 import { SparkleIcon, UserSecurityIcon } from '~/assets'
 
-function Developer() {
-  return (
+const roles = {
+  Developer: () => (
     <span className="group">
       <span className="font-mono">&lt;</span>探索者
       <span className="font-mono">/&gt;</span>
       <span className="invisible inline-flex text-zinc-300 before:content-['|'] group-hover:visible group-hover:animate-typing dark:text-zinc-500" />
     </span>
-  )
-}
-
-function Designer() {
-  return (
+  ),
+  Designer: () => (
     <span className="group relative bg-black/5 p-1 dark:bg-white/5">
       <span className="pointer-events-none absolute inset-0 border border-lime-700/90 opacity-70 group-hover:border-dashed group-hover:opacity-100 dark:border-lime-400/90">
-        <span className="absolute -left-[3.5px] -top-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
-        <span className="absolute -bottom-[3.5px] -right-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
-        <span className="absolute -bottom-[3.5px] -left-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
-        <span className="absolute -right-[3.5px] -top-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
+        {['-left-[3.5px] -top-[3.5px]', '-bottom-[3.5px] -right-[3.5px]', '-bottom-[3.5px] -left-[3.5px]', '-right-[3.5px] -top-[3.5px]'].map((pos) => (
+          <span key={pos} className={`absolute ${pos} size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400`} />
+        ))}
       </span>
       生活家
     </span>
-  )
-}
-
-function OCD() {
-  return (
+  ),
+  OCD: () => (
     <span className="group inline-flex items-center">
       <SparkleIcon className="mr-1 inline-flex transform-gpu transition-transform duration-500 group-hover:rotate-180" />
       <span>记录者</span>
     </span>
-  )
-}
-
-function Founder() {
-  return (
+  ),
+  Founder: () => (
     <span className="group inline-flex items-center">
       <UserSecurityIcon className="mr-1 inline-flex group-hover:fill-zinc-600/20 dark:group-hover:fill-zinc-200/20" />
       <span>思考者</span>
@@ -61,9 +51,13 @@ export function Headline() {
           duration: 0.3,
         }}
       >
-        <Developer />，<Designer />，
+        {Object.values(roles).map((Role, index) => (
+          <React.Fragment key={index}>
+            <Role />
+            {index < Object.values(roles).length - 1 && '，'}
+          </React.Fragment>
+        ))}
         <span className="block h-2" />
-        <OCD />，<Founder />
       </motion.h1>
       <motion.p
         className="mt-6 text-base text-zinc-600 dark:text-zinc-400"
@@ -78,9 +72,9 @@ export function Headline() {
         }}
       >
         <Balancer>
-    我是辛壬癸，一个热爱生活、喜欢记录和思考的人。
-    我的博客“辛壬癸的日常笔记”主要分享生活记录、命理研究和个人杂谈。
-    在这里，我通过文字记录生活的点滴，探索命理的奥秘，并分享我的见解和思考。
+          我是辛壬癸，一个热爱生活、喜欢记录和思考的人。
+          我的博客“辛壬癸的日常笔记”主要分享生活记录、命理研究和个人杂谈。
+          在这里，我通过文字记录生活的点滴，探索命理的奥秘，并分享我的见解和思考。
         </Balancer>
       </motion.p>
     </div>
