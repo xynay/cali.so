de7bcf43c6c9795ebd9f02dd48a2751d78a779e4
xynay <xiregiji@gmail.com>
2024-07-27 23:28:59 +0800

    Update Footer.tsx

--- app/(main)/Footer.tsx
+++ app/(main)/Footer.tsx
@@ -1,18 +1,11 @@
-import { count, isNotNull } from 'drizzle-orm'
 import Link from 'next/link'
 import React from 'react'
 
-import { CursorClickIcon, UsersIcon } from '~/assets'
 import { PeekabooLink } from '~/components/links/PeekabooLink'
 import { Container } from '~/components/ui/Container'
-import { kvKeys } from '~/config/kv'
 import { navigationItems } from '~/config/nav'
 import { db } from '~/db'
 import { subscribers } from '~/db/schema'
-import { env } from '~/env.mjs'
-import { prettifyNumber } from '~/lib/math'
-import { redis } from '~/lib/redis'
-
 import { Newsletter } from './Newsletter'
 
 function NavLink({
@@ -44,60 +37,6 @@ function Links() {
   )
 }
 
-async function TotalPageViews() {
-  let views: number
-  if (env.VERCEL_ENV === 'production') {
-    views = await redis.incr(kvKeys.totalPageViews)
-  } else {
-    views = 345678
-  }
-
-  return (
-    <span className="flex items-center justify-center gap-1 text-xs text-zinc-500 dark:text-zinc-400 md:justify-start">
-      <UsersIcon className="h-4 w-4" />
-      <span title={`${Intl.NumberFormat('en-US').format(views)}次浏览`}>
-        总浏览量&nbsp;
-        <span className="font-medium">{prettifyNumber(views, true)}</span>
-      </span>
-    </span>
-  )
-}
-
-type VisitorGeolocation = {
-  country: string
-  city?: string
-  flag: string
-}
-async function LastVisitorInfo() {
-  let lastVisitor: VisitorGeolocation | undefined = undefined
-  if (env.VERCEL_ENV === 'production') {
-    const [lv, cv] = await redis.mget<VisitorGeolocation[]>(
-      kvKeys.lastVisitor,
-      kvKeys.currentVisitor
-    )
-    lastVisitor = lv
-    await redis.set(kvKeys.lastVisitor, cv)
-  }
-
-  if (!lastVisitor) {
-    lastVisitor = {
-      country: 'US',
-      flag: '🇺🇸',
-    }
-  }
-
-  return (
-    <span className="flex items-center justify-center gap-1 text-xs text-zinc-500 dark:text-zinc-400 md:justify-start">
-      <CursorClickIcon className="h-4 w-4" />
-      <span>
-        最近访客来自&nbsp;
-        {[lastVisitor.city, lastVisitor.country].filter(Boolean).join(', ')}
-      </span>
-      <span className="font-medium">{lastVisitor.flag}</span>
-    </span>
-  )
-}
-
 export async function Footer() {
   const [subs] = await db
     .select({
@@ -122,16 +61,6 @@ export async function Footer() {
                 </PeekabooLink>
               </p>
               <Links />
-            </div>
-          </Container.Inner>
-          <Container.Inner className="mt-6">
-            <div className="flex flex-col items-center justify-start gap-2 sm:flex-row">
-              <React.Suspense>
-                <TotalPageViews />
-              </React.Suspense>
-              <React.Suspense>
-                <LastVisitorInfo />
-              </React.Suspense>
             </div>
           </Container.Inner>
         </div>
