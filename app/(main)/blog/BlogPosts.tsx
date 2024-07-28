254b504a52d707f8ba8f4c31e90a6aedb3346936
xynay <xiregiji@gmail.com>
2024-07-28 15:50:28 +0800

    Update BlogPosts.tsx

--- app/(main)/blog/BlogPosts.tsx
+++ app/(main)/blog/BlogPosts.tsx
@@ -1,17 +1,19 @@
-import { env } from '~/env.mjs';
-import { getLatestBlogPosts } from '~/sanity/queries';
-
-import { BlogPostCard } from './BlogPostCard';
 import { useMemo } from 'react';
 
+import { env } from '~/env.mjs';
+import { getLatestBlogPosts } from '~/sanity/queries';
+import { BlogPostCard } from './BlogPostCard';
+
 export async function BlogPosts({ limit = 5 }) {
-  const posts = await getLatestBlogPosts({ limit, forDisplay: true }) || [];
+  const fetchedPosts = await getLatestBlogPosts({ limit, forDisplay: true }) || [];
+
+  const posts = useMemo(() => fetchedPosts, [fetchedPosts]);
 
   const views = useMemo(() => 
     posts.map(() =>
       env.VERCEL_ENV === 'development' ? Math.floor(Math.random() * 1000) : 0
     ),
-    [posts] // 仅将 posts 作为依赖项
+    [posts]
   );
 
   return (
