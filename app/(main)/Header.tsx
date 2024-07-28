23c7f092891e1633156708c2dfc5915959599fd9
xynay <xiregiji@gmail.com>
2024-07-29 01:45:30 +0800

    Update Header.tsx

--- app/(main)/Header.tsx
+++ app/(main)/Header.tsx
@@ -31,13 +31,9 @@ import { Tooltip } from '~/components/ui/Tooltip'
 import { url } from '~/lib'
 import { clamp } from '~/lib/math'
 
-const updateStyles = (updateHeaderStyles, updateAvatarStyles) => {
-  updateHeaderStyles()
-  updateAvatarStyles()
-}
-
-export const Header = React.memo(() => {
+export function Header() {
   const isHomePage = usePathname() === '/'
+
   const headerRef = React.useRef<HTMLDivElement>(null)
   const avatarRef = React.useRef<HTMLDivElement>(null)
   const isInitial = React.useRef(true)
@@ -51,13 +47,14 @@ export const Header = React.memo(() => {
     const downDelay = avatarRef.current?.offsetTop ?? 0
     const upDelay = 64
 
-    const setProperty = (property, value) =>
+    const setProperty = (property: string, value: string | null) =>
       document.documentElement.style.setProperty(property, value)
-    const removeProperty = (property) =>
+    const removeProperty = (property: string) =>
       document.documentElement.style.removeProperty(property)
 
     const updateHeaderStyles = () => {
       if (!headerRef.current) return
+
       const { top, height } = headerRef.current.getBoundingClientRect()
       const scrollY = clamp(
         window.scrollY,
@@ -65,10 +62,7 @@ export const Header = React.memo(() => {
         document.body.scrollHeight - window.innerHeight
       )
 
-      if (isInitial.current) {
-        setProperty('--header-position', 'sticky')
-      }
-
+      if (isInitial.current) setProperty('--header-position', 'sticky')
       setProperty('--content-offset', `${downDelay}px`)
 
       if (isInitial.current || scrollY < downDelay) {
@@ -101,7 +95,6 @@ export const Header = React.memo(() => {
       const toScale = 36 / 64
       const fromX = 0
       const toX = 2 / 16
-
       const scrollY = downDelay - window.scrollY
 
       let scale = (scrollY * (fromScale - toScale)) / downDelay + toScale
@@ -121,15 +114,19 @@ export const Header = React.memo(() => {
       setProperty('--avatar-border-opacity', scale === toScale ? '1' : '0')
     }
 
-    const updateAllStyles = () => updateStyles(updateHeaderStyles, updateAvatarStyles)
+    const updateStyles = () => {
+      updateHeaderStyles()
+      updateAvatarStyles()
+      isInitial.current = false
+    }
 
-    updateAllStyles()
-    window.addEventListener('scroll', updateAllStyles, { passive: true })
-    window.addEventListener('resize', updateAllStyles)
+    updateStyles()
+    window.addEventListener('scroll', updateStyles, { passive: true })
+    window.addEventListener('resize', updateStyles)
 
     return () => {
-      window.removeEventListener('scroll', updateAllStyles)
-      window.removeEventListener('resize', updateAllStyles)
+      window.removeEventListener('scroll', updateStyles)
+      window.removeEventListener('resize', updateStyles)
     }
   }, [isHomePage])
 
@@ -266,31 +263,104 @@ export const Header = React.memo(() => {
                 className="flex justify-end gap-3 md:flex-1"
                 initial={{ opacity: 0, y: -20, scale: 0.95 }}
                 animate={{ opacity: 1, y: 0, scale: 1 }}
-                transition={{
-                  type: 'spring',
-                  damping: 30,
-                  stiffness: 200,
-                }}
               >
-                <SignedIn>
-                  <UserButton afterSignOutUrl={url('/')} />
-                </SignedIn>
-                <SignedOut>
-                  <SignInButton mode="modal">
-                    <Tooltip
-                      content="Login"
-                      className="pointer-events-auto relative z-50"
-                    >
-                      <MailIcon className="w-5 text-primary-600" />
-                    </Tooltip>
-                  </SignInButton>
-                </SignedOut>
-                <ThemeSwitcher className="pointer-events-auto relative z-50" />
+                <UserInfo />
+                <div className="pointer-events-auto">
+                  <ThemeSwitcher />
+                </div>
               </motion.div>
             </div>
           </Container>
         </div>
       </motion.header>
+      {isHomePage && <div className="h-[--content-offset]" />}
     </>
   )
-})
+}
+
+function UserInfo() {
+  const [tooltipOpen, setTooltipOpen] = React.useState(false)
+  const pathname = usePathname()
+  const { user } = useUser()
+  const StrategyIcon = React.useMemo(() => {
+    const strategy = user?.primaryEmailAddress?.verification.strategy
+    if (!strategy) return null
+
+    switch (strategy) {
+      case 'from_oauth_github':
+        return GitHubBrandIcon as (
+          props: React.ComponentProps<'svg'>
+        ) => JSX.Element
+      case 'from_oauth_google':
+        return GoogleBrandIcon
+      default:
+        return MailIcon
+    }
+  }, [user?.primaryEmailAddress?.verification.strategy])
+
+  return (
+    <AnimatePresence>
+      <SignedIn key="user-info">
+        <motion.div
+          className="pointer-events-auto relative flex h-10 items-center"
+          initial={{ opacity: 0, x: 25 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 25 }}
+        >
+          <UserButton
+            afterSignOutUrl={url(pathname).href}
+            appearance={{
+              elements: {
+                avatarBox: 'w-9 h-9 ring-2 ring-white/20',
+              },
+            }}
+          />
+          {StrategyIcon && (
+            <span className="pointer-events-none absolute -bottom-1 -right-1 flex h-4 w-4 select-none items-center justify-center rounded-full bg-white dark:bg-zinc-900">
+              <StrategyIcon className="h-3 w-3" />
+            </span>
+          )}
+        </motion.div>
+      </SignedIn>
+      <SignedOut key="sign-in">
+        <motion.div
+          className="pointer-events-auto"
+          initial={{ opacity: 0, x: 25 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 25 }}
+        >
+          <Tooltip.Provider disableHoverableContent>
+            <Tooltip.Root open={tooltipOpen} onOpenChange={setTooltipOpen}>
+              <SignInButton mode="modal" redirectUrl={url(pathname).href}>
+                <Tooltip.Trigger asChild>
+                  <button
+                    type="button"
+                    className="group h-10 rounded-full bg-gradient-to-b from-zinc-50/50 to-white/90 px-3 text-sm shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:from-zinc-900/50 dark:to-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
+                  >
+                    <UserArrowLeftIcon className="h-5 w-5" />
+                  </button>
+                </Tooltip.Trigger>
+              </SignInButton>
+
+              <AnimatePresence>
+                {tooltipOpen && (
+                  <Tooltip.Portal forceMount>
+                    <Tooltip.Content asChild>
+                      <motion.div
+                        initial={{ opacity: 0, scale: 0.96 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        exit={{ opacity: 0, scale: 0.95 }}
+                      >
+                        登录
+                      </motion.div>
+                    </Tooltip.Content>
+                  </Tooltip.Portal>
+                )}
+              </AnimatePresence>
+            </Tooltip.Root>
+          </Tooltip.Provider>
+        </motion.div>
+      </SignedOut>
+    </AnimatePresence>
+  )
+}
