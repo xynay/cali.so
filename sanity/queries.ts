7a161554cf94c2c8a0183e3117f12ad47fc1824e
xynay <xiregiji@gmail.com>
2024-07-28 14:46:12 +0800

    Update queries.ts

--- sanity/queries.ts
+++ sanity/queries.ts
@@ -1,36 +1,30 @@
-import { groq } from 'next-sanity'
-import { getDate } from '~/lib/date'
-import { client } from '~/sanity/lib/client'
-import { type Post, type PostDetail } from '~/sanity/schemas/post'
-import { type Project } from '~/sanity/schemas/project'
+import { groq } from 'next-sanity';
+import { getDate } from '~/lib/date';
+import { client } from '~/sanity/lib/client';
+import { type Post, type PostDetail } from '~/sanity/schemas/post';
+import { type Project } from '~/sanity/schemas/project';
 
 // 预先计算当前日期
-const currentDate = getDate().toISOString()
+const currentDate = getDate().toISOString();
 
-export const getAllLatestBlogPostSlugsQuery = () =>
-  groq`
-  *[_type == "post" && !(_id in path("drafts.**"))
-  && publishedAt <= $currentDate
-  && defined(slug.current)] | order(publishedAt desc).slug.current
-  `
+export const getAllLatestBlogPostSlugsQuery = () => groq`
+  *[_type == "post" && !(_id in path("drafts.**")) && publishedAt <= $currentDate && defined(slug.current)]
+  | order(publishedAt desc) { "slug": slug.current }
+`;
 
 export const getAllLatestBlogPostSlugs = () => {
-  return client.fetch<string[]>(getAllLatestBlogPostSlugsQuery(), { currentDate })
-}
+  return client.fetch<string[]>(getAllLatestBlogPostSlugsQuery(), { currentDate });
+};
 
 type GetBlogPostsOptions = {
-  limit?: number
-  offset?: number
-  forDisplay?: boolean
-}
+  limit?: number;
+  offset?: number;
+  forDisplay?: boolean;
+};
 
-export const getLatestBlogPostsQuery = ({
-  limit = 5,
-  forDisplay = true,
-}: GetBlogPostsOptions) =>
-  groq`
-  *[_type == "post" && !(_id in path("drafts.**")) && publishedAt <= $currentDate
-  && defined(slug.current)] | order(publishedAt desc)[0...${limit}] {
+export const getLatestBlogPostsQuery = ({ limit = 5, forDisplay = true }: GetBlogPostsOptions) => groq`
+  *[_type == "post" && !(_id in path("drafts.**")) && publishedAt <= $currentDate && defined(slug.current)]
+  | order(publishedAt desc) [0...${limit}] {
     _id,
     title,
     "slug": slug.current,
@@ -39,16 +33,17 @@ export const getLatestBlogPostsQuery = ({
     publishedAt,
     readingTime,
     mainImage {
-      _ref,
       asset->{
         url,
         ${forDisplay ? '"lqip": metadata.lqip, "dominant": metadata.palette.dominant,' : ''}
       }
     }
-  }`
+  }
+`;
 
-export const getLatestBlogPosts = (options: GetBlogPostsOptions) =>
-  client.fetch<Post[]>(getLatestBlogPostsQuery(options), { currentDate })
+export const getLatestBlogPosts = (options: GetBlogPostsOptions) => {
+  return client.fetch<Post[]>(getLatestBlogPostsQuery(options), { currentDate });
+};
 
 export const getBlogPostQuery = groq`
   *[_type == "post" && slug.current == $slug && !(_id in path("drafts.**"))][0] {
@@ -65,19 +60,17 @@ export const getBlogPostQuery = groq`
       _type == "image" => {
         "url": asset->url,
         "lqip": asset->metadata.lqip,
-        "dimensions": asset->metadata.dimensions,
-        ...
+        "dimensions": asset->metadata.dimensions
       }
     },
-    "headings": body[length(style) == 2 && string::startsWith(style, "h")],
     mainImage {
-      _ref,
       asset->{
         url,
         "lqip": metadata.lqip
       }
     },
-    "related": *[_type == "post" && slug.current != $slug && count(categories[@._ref in ^.^.categories[]._ref]) > 0] | order(publishedAt desc, _createdAt desc) [0..2] {
+    "related": *[_type == "post" && slug.current != $slug && count(categories[@._ref in ^.^.categories[]._ref]) > 0]
+    | order(publishedAt desc, _createdAt desc) [0...2] {
       _id,
       title,
       "slug": slug.current,
@@ -85,23 +78,21 @@ export const getBlogPostQuery = groq`
       publishedAt,
       readingTime,
       mainImage {
-        _ref,
         asset->{
           url,
           "lqip": metadata.lqip,
           "dominant": metadata.palette.dominant
         }
-      },
+      }
     }
-  }`
+  }
+`;
 
-export const getBlogPost = (slug: string) =>
-  client.fetch<PostDetail | undefined, { slug: string }>(getBlogPostQuery, {
-    slug,
-  })
+export const getBlogPost = (slug: string) => {
+  return client.fetch<PostDetail | undefined, { slug: string }>(getBlogPostQuery, { slug });
+};
 
-export const getSettingsQuery = () =>
-  groq`
+export const getSettingsQuery = () => groq`
   *[_type == "settings"][0] {
     "projects": projects[]->{
       _id,
@@ -118,17 +109,19 @@ export const getSettingsQuery = () =>
       end,
       "logo": logo.asset->url
     }
-  }`
+  }
+`;
 
-export const getSettings = () =>
-  client.fetch<{
-    projects: Project[] | null
-    heroPhotos?: string[] | null
+export const getSettings = () => {
+  return client.fetch<{
+    projects: Project[] | null;
+    heroPhotos?: string[] | null;
     resume?: {
-      company: string
-      title: string
-      logo: string
-      start: string
-      end?: string
-    }[] | null
-  }>(getSettingsQuery())
+      company: string;
+      title: string;
+      logo: string;
+      start: string;
+      end?: string;
+    }[] | null;
+  }>(getSettingsQuery());
+};
