a13ed9d44713995c8fd7fe1f1e46a848d3a4eef7
xynay <xiregiji@gmail.com>
2024-07-28 14:43:55 +0800

    Update BlogPosts.tsx

--- app/(main)/blog/BlogPosts.tsx
+++ app/(main)/blog/BlogPosts.tsx
@@ -1,25 +1,20 @@
-import { env } from '~/env.mjs'
-import { getLatestBlogPosts } from '~/sanity/queries'
+import { env } from '~/env.mjs';
+import { getLatestBlogPosts } from '~/sanity/queries';
 
-import { BlogPostCard } from './BlogPostCard'
+import { BlogPostCard } from './BlogPostCard';
 
 export async function BlogPosts({ limit = 5 }) {
-  const posts = await getLatestBlogPosts({ limit, forDisplay: true }) || []
+  const posts = await getLatestBlogPosts({ limit, forDisplay: true }) || [];
 
-  let views: number[] = []
-  if (env.VERCEL_ENV === 'development') {
-    views = posts.map(() => Math.floor(Math.random() * 1000))
-  } else {
-    // Since Redis is removed, we no longer fetch views from Redis
-    // Initialize with default values
-    views = posts.map(() => 0)
-  }
+  const views = posts.map(() =>
+    env.VERCEL_ENV === 'development' ? Math.floor(Math.random() * 1000) : 0
+  );
 
   return (
     <>
       {posts.map((post, idx) => (
-        <BlogPostCard post={post} views={views[idx] ?? 0} key={post._id} />
+        <BlogPostCard post={post} views={views[idx]} key={post._id} />
       ))}
     </>
-  )
+  );
 }
