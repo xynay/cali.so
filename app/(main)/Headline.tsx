31edeaa1c74c1e38718216d55cd8d609dde32fa1
xynay <xiregiji@gmail.com>
2024-07-29 16:24:07 +0800

    Update Headline.tsx

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -13,7 +13,7 @@ const borderPositions = [
 ]
 
 const Developer = () => (
-  <span className="group text-2xl">
+  <span className="group text-5xl">
     <span className="font-mono">&lt;</span>探索者
     <span className="font-mono">/&gt;</span>
     <span className="invisible inline-flex text-zinc-300 before:content-['|'] group-hover:visible group-hover:animate-typing dark:text-zinc-500" />
@@ -21,7 +21,7 @@ const Developer = () => (
 )
 
 const Designer = () => (
-  <span className="group relative bg-black/5 p-1 dark:bg-white/5 text-2xl">
+  <span className="group relative bg-black/5 p-1 dark:bg-white/5 text-5xl">
     <span className="pointer-events-none absolute inset-0 border border-lime-700/90 opacity-70 group-hover:border-dashed group-hover:opacity-100 dark:border-lime-400/90">
       {borderPositions.map((pos) => (
         <span key={pos} className={`absolute ${pos} size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400`} />
@@ -32,14 +32,14 @@ const Designer = () => (
 )
 
 const OCD = () => (
-  <span className="group inline-flex items-center text-2xl">
+  <span className="group inline-flex items-center text-5xl">
     <SparkleIcon className="mr-1 inline-flex transform-gpu transition-transform duration-500 group-hover:rotate-180" />
     <span>记录者</span>
   </span>
 )
 
 const Founder = () => (
-  <span className="group inline-flex items-center text-2xl">
+  <span className="group inline-flex items-center text-6xl">
     <UserSecurityIcon className="mr-1 inline-flex group-hover:fill-zinc-600/20 dark:group-hover:fill-zinc-200/20" />
     <span>思考者</span>
   </span>
