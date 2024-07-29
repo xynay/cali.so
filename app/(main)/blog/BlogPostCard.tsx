2b2b47a95b50953266b5a7a8a6a985392f03afb6
xynay <xiregiji@gmail.com>
2024-07-29 18:36:28 +0800

    Update BlogPostCard.tsx

--- app/(main)/blog/BlogPostCard.tsx
+++ app/(main)/blog/BlogPostCard.tsx
@@ -1,7 +1,6 @@
 import { parseDateTime } from '@zolplay/utils'
 import Image from 'next/image'
 import Link from 'next/link'
-
 import {
   CalendarIcon,
   HourglassIcon,
@@ -16,6 +15,7 @@ function BlogPostCard({ post }: { post: Post }) {
   const imageUrl = mainImage.asset.url
   const lqip = mainImage.asset.lqip
   const formattedDate = parseDateTime({ date: new Date(publishedAt) })?.format('YYYY/MM/DD')
+
   return (
     <Link
       href={`/blog/${slug}`}
@@ -38,6 +38,7 @@ function BlogPostCard({ post }: { post: Post }) {
           blurDataURL={lqip}
           fill
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
+          loading="lazy"
         />
       </div>
       <span className="relative z-10 flex w-full flex-1 shrink-0 flex-col justify-between gap-0.5 rounded-b-[calc(1.5rem+1px)] bg-cover bg-bottom bg-no-repeat p-4 bg-blend-overlay [background-image:var(--post-image)] before:pointer-events-none before:absolute before:inset-0 before:z-10 before:select-none before:rounded-b-[calc(1.5rem-1px)] before:bg-[--post-image-bg] before:opacity-70 before:transition-opacity after:pointer-events-none after:absolute after:inset-0 after:z-10 after:select-none after:rounded-b-[calc(1.5rem-1px)] after:bg-gradient-to-b after:from-transparent after:to-[--post-image-bg] after:backdrop-blur after:transition-opacity group-hover:before:opacity-30 md:p-5">
