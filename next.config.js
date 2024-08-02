eb7ade51e923162d38df0a8db6be9c0e8fc37c18
xynay <xiregiji@gmail.com>
2024-08-03 02:35:51 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+if (!process.env.SKIP_ENV_VALIDATION) {
+  (async () => {
+    await import('./env.mjs');
+  })();
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -13,24 +22,6 @@ const nextConfig = {
   experimental: {
     taint: true,
   },
-  redirects() {
-    return [
-      { source: '/twitter', destination: 'https://x.com/thecalicastle', permanent: false },
-      { source: '/x', destination: 'https://x.com/thecalicastle', permanent: false },
-      { source: '/youtube', destination: 'https://youtube.com/@calicastle', permanent: false },
-      { source: '/tg', destination: 'https://t.me/cali_so', permanent: false },
-      { source: '/linkedin', destination: 'https://www.linkedin.com/in/calicastle/', permanent: false },
-      { source: '/github', destination: 'https://github.com/CaliCastle', permanent: false },
-      { source: '/bilibili', destination: 'https://space.bilibili.com/8350251', permanent: false },
-    ];
-  },
-  rewrites() {
-    return [
-      { source: '/feed', destination: '/feed.xml' },
-      { source: '/rss', destination: '/feed.xml' },
-      { source: '/rss.xml', destination: '/feed.xml' },
-    ];
-  },
   webpack(config, { dev, isServer }) {
     if (!dev && !isServer) {
       config.optimization.minimizer.push(
@@ -38,41 +29,26 @@ const nextConfig = {
           terserOptions: {
             compress: {
               drop_console: true,
-              drop_debugger: true,
-              pure_funcs: ['console.info', 'console.debug'],
-            },
-            format: {
-              comments: false,
             },
           },
-          extractComments: false,
         }),
         new CssMinimizerPlugin()
       );
     }
     config.cache = {
       type: 'filesystem',
-      buildDependencies: {
-        config: [__filename],
-      },
-      name: 'my-cache',
     };
     config.optimization.splitChunks = {
       chunks: 'all',
-      minSize: 20000,
-      maxSize: 0,
       cacheGroups: {
         vendors: {
           test: /[\\/]node_modules[\\/]/,
           priority: -10,
-          reuseExistingChunk: true,
-          filename: 'vendors.[contenthash].js',
         },
         default: {
           minChunks: 2,
           priority: -20,
           reuseExistingChunk: true,
-          filename: 'common.[contenthash].js',
         },
       },
     };
