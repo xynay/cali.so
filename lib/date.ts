204fbf81f8ff58ce6eca6b38bfc287e423a26adc
xynay <xiregiji@gmail.com>
2024-08-02 19:43:40 +0800

    Update date.ts

--- lib/date.ts
+++ lib/date.ts
@@ -1,10 +1,18 @@
-import dayjs from 'dayjs'
-import timezone from 'dayjs/plugin/timezone'
-import utc from 'dayjs/plugin/utc'
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 
-dayjs.extend(utc)
-dayjs.extend(timezone)
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+let cachedDate: dayjs.Dayjs | null = null;
+let cachedTimezone: string | null = null;
 
 export function getDate(timezone = 'Asia/Shanghai'): dayjs.Dayjs {
-  return dayjs().tz(timezone)
+  if (cachedDate && cachedTimezone === timezone) {
+    return cachedDate;
+  }
+  cachedDate = dayjs().tz(timezone);
+  cachedTimezone = timezone;
+  return cachedDate;
 }
