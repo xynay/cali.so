715ac81f4e37a8c61484500d5b5646389d488be2
xynay <xiregiji@gmail.com>
2024-08-02 15:39:18 +0800

    Update BlogPosts.tsx

--- app/(main)/blog/BlogPosts.tsx
+++ app/(main)/blog/BlogPosts.tsx
@@ -1,22 +1,9 @@
-"use client";
-
-import React, { useEffect, useState } from 'react';
 import { getLatestBlogPosts } from '~/sanity/queries';
+
 import { BlogPostCard } from './BlogPostCard';
 
-const BlogPosts = React.memo(({ limit = 5 }) => {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const fetchedPosts = await getLatestBlogPosts({ limit, forDisplay: true }) || [];
-      setPosts(fetchedPosts);
-    };
-
-    fetchPosts().catch(error => {
-      console.error('Failed to fetch posts:', error);
-    });
-  }, [limit]);
+export async function BlogPosts({ limit = 5 }) {
+  const posts = await getLatestBlogPosts({ limit, forDisplay: true }) || [];
 
   return (
     <>
@@ -25,6 +12,4 @@ const BlogPosts = React.memo(({ limit = 5 }) => {
       ))}
     </>
   );
-});
-
-export default BlogPosts;
+}
