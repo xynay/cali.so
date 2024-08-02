4c5df3ad1b6e04b0a7c8738a45202fe43c0363b0
xynay <xiregiji@gmail.com>
2024-08-02 15:47:10 +0800

    Update Container.tsx

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -1,20 +1,23 @@
-import { clsxm } from '@zolplay/utils'
-import React from 'react'
+import { clsxm } from '@zolplay/utils';
+import React from 'react';
 
-type ContainerProps = React.ComponentPropsWithoutRef<'div'>
+type ContainerProps = React.ComponentPropsWithoutRef<'div'>;
 
-const OuterContainer = React.forwardRef<HTMLDivElement, ContainerProps>(
-  function OuterContainer({ className, children, ...props }, ref) {
+const OuterContainer = React.memo(
+  React.forwardRef<HTMLDivElement, ContainerProps>(function OuterContainer(
+    { className, children, ...props },
+    ref
+  ) {
     return (
       <div ref={ref} className={clsxm('sm:px-8', className)} {...props}>
         <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
       </div>
-    )
-  }
-)
+    );
+  })
+);
 
-const InnerContainer = React.forwardRef<HTMLDivElement, ContainerProps>(
-  function InnerContainer(
+const InnerContainer = React.memo(
+  React.forwardRef<HTMLDivElement, ContainerProps>(function InnerContainer(
     { className, children, ...props }: ContainerProps,
     ref
   ) {
@@ -26,9 +29,9 @@ const InnerContainer = React.forwardRef<HTMLDivElement, ContainerProps>(
       >
         <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
       </div>
-    )
-  }
-)
+    );
+  })
+);
 
 const ContainerComponent = React.forwardRef<HTMLDivElement, ContainerProps>(
   function Container({ children, ...props }: ContainerProps, ref) {
@@ -36,11 +39,11 @@ const ContainerComponent = React.forwardRef<HTMLDivElement, ContainerProps>(
       <OuterContainer ref={ref} {...props}>
         <InnerContainer>{children}</InnerContainer>
       </OuterContainer>
-    )
+    );
   }
-)
+);
 
 export const Container = Object.assign(ContainerComponent, {
   Outer: OuterContainer,
   Inner: InnerContainer,
-})
+});
