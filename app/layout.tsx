192b966e6265d80791723a427fbf3e66361d9dca
xynay <xiregiji@gmail.com>
2024-08-03 21:18:59 +0800

    0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,67 @@
+import './globals.css'; // 导入包含其他 CSS 文件的全局样式
+
 import { ClerkProvider } from '@clerk/nextjs';
-import Script from 'next/script';
+import type { Metadata, Viewport } from 'next';
 
 import { ThemeProvider } from '~/app/(main)/ThemeProvider';
+import { url } from '~/lib';
 import { zhCN } from '~/lib/clerkLocalizations';
 import { sansFont } from '~/lib/font';
+import { seo } from '~/lib/seo';
+
+// Metadata Configuration
+export const metadata: Metadata = {
+  metadataBase: seo.url,
+  title: {
+    template: '%s | 辛壬癸的命理笔记',
+    default: seo.title,
+  },
+  description: seo.description,
+  keywords: '生活记录,命理研究,杂谈,个人博客,辛壬癸,日常笔记,命运解析,生活智慧,命理学,日常生活',
+  manifest: '/site.webmanifest',
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  openGraph: {
+    title: {
+      default: seo.title,
+      template: '%s | 辛壬癸的命理笔记',
+    },
+    description: seo.description,
+    siteName: '辛壬癸的命理笔记',
+    locale: 'zh_CN',
+    type: 'website',
+    url: 'https://xinrengui.eu.org',
+  },
+  twitter: {
+    site: '@thecalicastle',
+    creator: '@thecalicastle',
+    card: 'summary_large_image',
+    title: seo.title,
+    description: seo.description,
+  },
+  alternates: {
+    canonical: url('/'),
+    types: {
+      'application/rss+xml': [{ url: 'rss', title: 'RSS 订阅' }],
+    },
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: dark)', color: '#000212' },
+    { media: '(prefers-color-scheme: light)', color: '#fafafa' },
+  ],
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -14,31 +72,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         suppressHydrationWarning
       >
         <head>
-          <Script
-            id="load-css"
-            strategy="beforeInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function() {
-                  var links = [
-                    '/globals.css',
-                    '/clerk.css',
-                    '/prism.css'
-                  ];
-                  links.forEach(function(href) {
-                    var link = document.createElement('link');
-                    link.rel = 'stylesheet';
-                    link.href = href;
-                    link.media = 'print'; // 初始设置为 print 以避免阻塞渲染
-                    link.onload = function() {
-                      link.media = 'all'; // 加载完成后设置为 all
-                    };
-                    document.head.appendChild(link);
-                  });
-                })();
-              `,
-            }}
-          />
+          {/* Removed the preload link for 1.woff2 */}
         </head>
         <body className="flex h-full flex-col">
           <ThemeProvider
