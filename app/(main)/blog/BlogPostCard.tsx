102908220ca9e7ec2d691c06a5142abe8d5fc9fb
xynay <xiregiji@gmail.com>
2024-08-05 00:42:31 +0800

    Update BlogPostCard.tsx

--- app/(main)/blog/BlogPostCard.tsx
+++ app/(main)/blog/BlogPostCard.tsx
@@ -9,19 +9,18 @@ import { type Post } from '~/sanity/schemas/post';
 const BlogPostCard = memo(({ post }: { post: Post }) => {
   const { title, slug, mainImage, publishedAt, categories, readingTime } = post;
   const { dominant: { foreground, background } = {}, url: imageUrl, lqip } = mainImage.asset;
-
   const formattedDate = parseDateTime({ date: new Date(publishedAt) })?.format('YYYY/MM/DD');
 
   return (
     <Link
       href={`/blog/${slug}`}
       prefetch={false}
-      className="group relative flex w-full transform-gpu flex-col rounded-3xl bg-transparent ring-2 ring-[--post-image-bg] transition-transform hover:-translate-y-0.5"
+      className="group relative flex w-full flex-col rounded-3xl bg-transparent ring-2 ring-[--post-image-bg] transition-transform hover:-translate-y-0.5"
       style={{
         '--post-image-fg': foreground,
         '--post-image-bg': background,
         '--post-image': `url(${imageUrl})`,
-      } as React.CSSProperties}
+      }}
     >
       <div className="relative aspect-[240/135] w-full">
         <Image
@@ -35,10 +34,10 @@ const BlogPostCard = memo(({ post }: { post: Post }) => {
           height={135}
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
           loading="lazy"
-          priority // 优先加载关键图片
+          priority
         />
       </div>
-      <span className="relative z-10 flex w-full flex-1 shrink-0 flex-col justify-between gap-0.5 rounded-b-[calc(1.5rem+1px)] bg-cover bg-bottom bg-no-repeat p-4 bg-blend-overlay [background-image:var(--post-image)] before:pointer-events-none before:absolute before:inset-0 before:z-10 before:select-none before:rounded-b-[calc(1.5rem-1px)] before:bg-[--post-image-bg] before:opacity-70 before:transition-opacity after:pointer-events-none after:absolute after:inset-0 after:z-10 after:select-none after:rounded-b-[calc(1.5rem-1px)] after:bg-gradient-to-b after:from-transparent after:to-[--post-image-bg] after:backdrop-blur after:transition-opacity group-hover:before:opacity-30 md:p-5">
+      <span className="relative z-10 flex w-full flex-1 flex-col justify-between p-4 bg-cover bg-bottom bg-no-repeat rounded-b-[calc(1.5rem+1px)] bg-blend-overlay [background-image:var(--post-image)] before:absolute before:inset-0 before:rounded-b-[calc(1.5rem-1px)] before:bg-[--post-image-bg] before:opacity-70 group-hover:before:opacity-30 md:p-5">
         <h2 className="z-20 text-base font-bold tracking-tight text-[--post-image-fg] opacity-70 transition-opacity group-hover:opacity-100 md:text-xl">
           {title}
         </h2>
