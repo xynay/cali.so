5d7505a32330061c302bf6553d5790ae124387ce
xynay <xiregiji@gmail.com>
2024-08-04 12:21:21 +0800

    Update BlogPostCard.tsx

--- app/(main)/blog/BlogPostCard.tsx
+++ app/(main)/blog/BlogPostCard.tsx
@@ -35,6 +35,7 @@ const BlogPostCard = memo(({ post }: { post: Post }) => {
           height={135}
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
           loading="lazy"
+          priority // 优先加载关键图片
         />
       </div>
       <span className="relative z-10 flex w-full flex-1 shrink-0 flex-col justify-between gap-0.5 rounded-b-[calc(1.5rem+1px)] bg-cover bg-bottom bg-no-repeat p-4 bg-blend-overlay [background-image:var(--post-image)] before:pointer-events-none before:absolute before:inset-0 before:z-10 before:select-none before:rounded-b-[calc(1.5rem-1px)] before:bg-[--post-image-bg] before:opacity-70 before:transition-opacity after:pointer-events-none after:absolute after:inset-0 after:z-10 after:select-none after:rounded-b-[calc(1.5rem-1px)] after:bg-gradient-to-b after:from-transparent after:to-[--post-image-bg] after:backdrop-blur after:transition-opacity group-hover:before:opacity-30 md:p-5">
