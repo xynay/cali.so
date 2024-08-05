b1bf119a646927a8c386f0837db6d4a130dfdd16
xynay <xiregiji@gmail.com>
2024-08-05 13:58:12 +0800

    0

--- app/(main)/Footer.tsx
+++ app/(main)/Footer.tsx
@@ -1,8 +1,6 @@
-'use client'
-
 import { count, isNotNull } from 'drizzle-orm'
 import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import { PeekabooLink } from '~/components/links/PeekabooLink'
 import { Container } from '~/components/ui/Container'
@@ -12,12 +10,7 @@ import { subscribers } from '~/db/schema'
 
 import Newsletter from './Newsletter'
 
-interface NavLinkProps {
-  href: string
-  children: React.ReactNode
-}
-
-const NavLink = React.memo(({ href, children }: NavLinkProps) => {
+const NavLink = React.memo(({ href, children }: { href: string; children: React.ReactNode }) => {
   return (
     <Link href={href} className="transition hover:text-lime-500 dark:hover:text-lime-400">
       {children}
@@ -39,30 +32,13 @@ const Links = React.memo(() => {
 })
 Links.displayName = 'Links'
 
-const Footer = () => {
-  const [subCount, setSubCount] = useState('0')
-
-  useEffect(() => {
-    const fetchSubCount = async () => {
-      try {
-        const [subs] = await db
-          .select({
-            subCount: count(),
-          })
-          .from(subscribers)
-          .where(isNotNull(subscribers.subscribedAt))
-        
-        setSubCount(subs?.subCount.toString() ?? '0')
-      } catch (error) {
-        console.error('Failed to fetch subscriber count:', error)
-      }
-    }
-
-    // 使用立即执行的异步函数 (IIFE)
-    void (async () => {
-      await fetchSubCount()
-    })()
-  }, [])
+export async function Footer() {
+  const [subs] = await db
+    .select({
+      subCount: count(),
+    })
+    .from(subscribers)
+    .where(isNotNull(subscribers.subscribedAt))
 
   return (
     <footer className="mt-32">
@@ -70,7 +46,7 @@ const Footer = () => {
         <div className="border-t border-zinc-100 pb-16 pt-10 dark:border-zinc-700/40">
           <Container.Inner>
             <div className="mx-auto mb-8 max-w-md">
-              <Newsletter subCount={subCount} />
+              <Newsletter subCount={`${subs?.subCount ?? '0'}`} />
             </div>
             <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
               <p className="text-sm text-zinc-500/80 dark:text-zinc-400/80">
@@ -87,5 +63,3 @@ const Footer = () => {
     </footer>
   )
 }
-
-export default Footer
